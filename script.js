88d6d9c383f47285ab91f629c99220b68695725a
e255558 <e255558@edu.sbl.ch>
2023-12-05 10:25:03 +0100

    aufgabe 3,4,5

--- script.js
+++ script.js
@@ -18,3 +18,36 @@ export function aufgabe02(args) {
   const result = input.toUpperCase()
   return result
 }
+
+export function aufgabe03(args){
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+  const currentElement = input [i]
+  if (currentElement ==="e" || currentElement ==="E") {
+    result++
+  }
+ }
+  return result
+}
+
+export function aufgabe04(args){
+    const cleanText = args.replace(/[^\w\s]/gi, '')
+    const words = cleanText.split(/\s+/);
+
+    return words.length;
+
+}
+
+export function aufgabe05(args) {
+  const input = args
+  if (input.toLowerCase() === input) {
+    return false
+  } else {
+    return true
+  }
+  }
+
+
+
